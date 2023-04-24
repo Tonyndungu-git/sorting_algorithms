126c81495e0d2b163808594d93eb99fb972a1208
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-04-24 16:19:15 +0300

    shell sort algorithm

--- sort.h
+++ sort.h
@@ -29,6 +29,7 @@ void swap(int *a, int *b);
 int partition(int *array, int low, int high, size_t size);
 void quickalg(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 
 #endif
