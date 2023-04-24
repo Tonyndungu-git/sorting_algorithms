fb3e5d1ed85c527d60e480207625d5dd9b71662a
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-04-24 10:38:46 +0300

    bubblee sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - function that sorts an array by using bubble sort algorithm.
+ * @array: array to be sorted.
+ * @size: size of the array.
+ * Return: Void.
+ */
 
 void bubble_sort(int *array, size_t size)
 {
@@ -21,23 +27,10 @@ void bubble_sort(int *array, size_t size)
 			}
 
 		}
-		if (array[size -1] < array[size -2])
+		if (array[size - 1] < array[size - 2])
 			return;
 
 	}
 
 
-}
-
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
 }
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+
+
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current = (*list)
+
+
+
+
+
+
+}
